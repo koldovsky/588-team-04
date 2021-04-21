5c15582376253092e17ecd1c1e6cd759add22b90
maradon1995 <friclerdenis1995@gmail.com>
2021-04-21 22:41:14 +0000

    js coding

---
+++ js/burger-menu.js
@@ -0,0 +1,25 @@
+
+
+
+// Burger
+const iconMenu = document.querySelector('.menu_icon');
+const menuBody = document.querySelector('.menu');
+if(iconMenu){
+    iconMenu.addEventListener("click", function (e) {
+        document.body.classList.toggle('_lock');
+        iconMenu.classList.toggle('_active');
+        menuBody.classList.toggle('_active');
+    });
+}   
+
+
+ 
+
+if(iconMenu.classList.contains('_active')) {
+                document.body.classList.remove('_lock');
+                iconMenu.classList.remove('_active');
+                menuBody.classList.remove('_active');
+            }
+
+
+
