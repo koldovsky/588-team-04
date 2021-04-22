0ed575a17073106cc7d0cb1e5eb9bdfad338df2b
maradon1995 <friclerdenis1995@gmail.com>
2021-04-22 22:01:05 +0300

    add js

--- js/burger-menu.js
+++ js/burger-menu.js
@@ -1,25 +1,30 @@
-
-
-
 // Burger
 const iconMenu = document.querySelector('.menu_icon');
 const menuBody = document.querySelector('.menu');
-if(iconMenu){
-    iconMenu.addEventListener("click", function (e) {
-        document.body.classList.toggle('_lock');
-        iconMenu.classList.toggle('_active');
-        menuBody.classList.toggle('_active');
-    });
-}   
+if (iconMenu.classList.contains()) {
+  iconMenu.addEventListener('click', function (e) {
+    document.body.classList.add('_lock');
+    iconMenu.classList.add('_active');
+    menuBody.classList.add('_active');
+  });
+}
 
 
- 
-
-if(iconMenu.classList.contains('_active')) {
-                document.body.classList.remove('_lock');
-                iconMenu.classList.remove('_active');
-                menuBody.classList.remove('_active');
-            }
+/*document.querySelectorAll('.menu a')
+.forEach( menuItem => menuItem.addEventListener('click', hideMenu));*/
 
 
+const list = document.querySelectorAll(".menu a");
+list.forEach(i => {
+    i.addEventListener('click', function (hideMenu) {
+        hideMenu.preventDefault();
+        document.querySelector('.menu').style.display = 'none';
+    })
+});
 
+
+/*if (iconMenu.classList.contains('_active')) {
+  document.body.classList.remove('_lock');
+  iconMenu.classList.remove('_active');
+  menuBody.classList.remove('_active');
+}*/
