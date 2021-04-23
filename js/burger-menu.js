63b4b5394872d39936aebdf8c5b605a4fdf3a7e3
maradon1995 <friclerdenis1995@gmail.com>
2021-04-23 23:05:34 +0300

    use some code in js and html

--- js/burger-menu.js
+++ js/burger-menu.js
@@ -2,24 +2,25 @@
 const iconMenu = document.querySelector('.menu_icon');
 const menuBody = document.querySelector('.menu');
 const menuSocialIcons = document.querySelector('.social-networks-fa-burger');
-if (iconMenu) {
-  iconMenu.addEventListener('click', function (e) {
-    document.body.classList.toggle('_lock');
-    iconMenu.classList.toggle('_active');
-    menuBody.classList.toggle('_active');
-    menuSocialIcons.classList.toggle('_active');
-  });
-}
+const tel = document.querySelector('.phonecell');
 
-document.querySelectorAll('.menu a')
+iconMenu.addEventListener('click', function (e) {
+  document.body.classList.toggle('_lock');
+  iconMenu.classList.toggle('_active');
+  menuBody.classList.toggle('_active');
+  menuSocialIcons.classList.toggle('_active');
+  tel.classList.toggle('_active');
+});
+
+document
+  .querySelectorAll('.menu a')
   .forEach((menuItem) => menuItem.addEventListener('click', hideMenu));
 
+  //Burger close after click
 function hideMenu() {
-  if (iconMenu.classList.contains('_active')) {
-    document.body.classList.remove('_lock');
-    iconMenu.classList.remove('_active');
-    menuBody.classList.remove('_active');
-    menuSocialIcons.classList.remove('_active');
-  }
+  document.body.classList.remove('_lock');
+  iconMenu.classList.remove('_active');
+  menuBody.classList.remove('_active');
+  menuSocialIcons.classList.remove('_active');
+  tel.classList.remove('_active');
 }
-
