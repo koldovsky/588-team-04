c55e968a937fc7d94c85219177cb81da5f4d917e
boobobobobo <h69463464@gmail.com>
2021-04-29 18:25:05 +0000

    code finished

--- js/carousel-slide.js
+++ js/carousel-slide.js
@@ -1,3 +1,4 @@
+(function () {
 let slideIndex = 1;
 showSlides(slideIndex);
 
@@ -28,3 +29,4 @@ function showSlides(n) {
   slides[slideIndex - 1].style.display = 'block';
   dots[slideIndex - 1].className += ' active';
 }
+})();
