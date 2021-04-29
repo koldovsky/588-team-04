baff71ccbac06532d14ccca31e274daa1c974383
maradon1995 <friclerdenis1995@gmail.com>
2021-04-29 12:22:42 +0300

    fixed bugs

--- js/burger-menu.js
+++ js/burger-menu.js
@@ -1,26 +1,26 @@
-// Burger
-const iconMenu = document.querySelector('.menu_icon');
-const menuBody = document.querySelector('.menu');
-const menuSocialIcons = document.querySelector('.social-networks-fa-burger');
-const tel = document.querySelector('.phonecell');
+(function () {
+  const iconMenu = document.querySelector('.menu_icon');
+  const menuBody = document.querySelector('.menu');
+  const menuSocialIcons = document.querySelector('.social-networks-fa-burger');
+  const tel = document.querySelector('.phonecell');
 
-iconMenu.addEventListener('click', function (e) {
-  document.body.classList.toggle('_lock');
-  iconMenu.classList.toggle('_active');
-  menuBody.classList.toggle('_active');
-  menuSocialIcons.classList.toggle('_active');
-  tel.classList.toggle('_active');
-});
+  iconMenu.addEventListener('click', function (e) {
+    document.body.classList.toggle('_lock');
+    iconMenu.classList.toggle('_active');
+    menuBody.classList.toggle('_active');
+    menuSocialIcons.classList.toggle('_active');
+    tel.classList.toggle('_active');
+  });
 
-document
-  .querySelectorAll('.menu a')
-  .forEach((menuItem) => menuItem.addEventListener('click', hideMenu));
+  document
+    .querySelectorAll('.menu a')
+    .forEach((menuItem) => menuItem.addEventListener('click', hideMenu));
 
-  //Burger close after click
-function hideMenu() {
-  document.body.classList.remove('_lock');
-  iconMenu.classList.remove('_active');
-  menuBody.classList.remove('_active');
-  menuSocialIcons.classList.remove('_active');
-  tel.classList.remove('_active');
-}
+  function hideMenu() {
+    document.body.classList.remove('_lock');
+    iconMenu.classList.remove('_active');
+    menuBody.classList.remove('_active');
+    menuSocialIcons.classList.remove('_active');
+    tel.classList.remove('_active');
+  }
+})();
--- js/slide-show.js
+++ js/slide-show.js
@@ -1,33 +1,33 @@
-let slideIndex = 1;
-showSlides(slideIndex);
+(function () {
+  let slideIndex = 1;
+  showSlides(slideIndex);
 
-function plusSlide() {
-  showSlides((slideIndex += 1));
-}
-
-function minusSlide() {
-  showSlides((slideIndex -= 1));
-}
-
-function currentSlide(n) {
-  showSlides((slideIndex = n));
-}
-
-function showSlides(n) {
-  let slides = document.getElementsByClassName('my-slides');
-  if (n > slides.length) {
-    slideIndex = 1;
+  function plusSlide() {
+    showSlides((slideIndex += 1));
   }
-  if (n < 1) {  
-    slideIndex = slides.length;
+
+  function minusSlide() {
+    showSlides((slideIndex -= 1));
   }
-  for (let i = 0; i < slides.length; i++) {
-    slides[i].style.display = 'none';
+
+  function currentSlide(n) {
+    showSlides((slideIndex = n));
   }
-  slides[slideIndex - 1].style.display = 'block';
-}
 
+  function showSlides(n) {
+    let slides = document.getElementsByClassName('my-slides');
+    if (n > slides.length) {
+      slideIndex = 1;
+    }
+    if (n < 1) {
+      slideIndex = slides.length;
+    }
+    for (let i = 0; i < slides.length; i++) {
+      slides[i].style.display = 'none';
+    }
+    slides[slideIndex - 1].style.display = 'block';
+  }
 
-
-document.querySelector('.prev').addEventListener('click', minusSlide);
-document.querySelector('.next').addEventListener('click', plusSlide);
+  document.querySelector('.prev').addEventListener('click', minusSlide);
+  document.querySelector('.next').addEventListener('click', plusSlide);
+})();
